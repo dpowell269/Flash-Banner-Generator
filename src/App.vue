c996a24870a277d5545e2995f2b3f6892b22e8f5
dpowell269 <dpowell@theperfumeshop.com>
2024-11-21 14:49:58 +0000

    Fix folder structure.

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+import FlashSaleStaggered from './components/FlashSaleStaggered.vue'
+</script>
+
+<template>
+  
+
+  <main>
+    <FlashSaleStaggered />
+  </main>
+</template>
+
