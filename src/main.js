2672ec441ea7377603ead88c34a275b25bd71496
dpowell269 <dpowell@theperfumeshop.com>
2024-12-04 16:28:34 +0000

    Remove splide.

--- src/main.js
+++ src/main.js
@@ -3,9 +3,8 @@ import './assets/main.css'
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import VueSplide from '@splidejs/vue-splide';
+
 
 createApp(App)
   .use(router)
-  .use( VueSplide )
   .mount('#app');
